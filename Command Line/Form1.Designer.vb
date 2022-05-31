2cc4e20a433cd23958776408821ecc2617cec8c3
Jordan Gray <business@jordantgray.uk>
2022-05-31 18:26:04 +0100

    Add command line

---
+++ Command Line/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(-2, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(382, 235)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Black
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.ForeColor = System.Drawing.Color.White
+        Me.TextBox2.Location = New System.Drawing.Point(45, 245)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(332, 14)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Location = New System.Drawing.Point(109, 109)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(380, 265)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(3, 245)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = ">"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Navy
+        Me.ClientSize = New System.Drawing.Size(593, 480)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Command Line/Form1.vb
@@ -0,0 +1,102 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.KeyPreview = True
+        CurrentCommand(1) = "ShinkOS"
+        Dim x As Process = Process.GetCurrentProcess()
+        writeNew("ShinkOS Console test - Ver 0.0.1", False, False)
+        writeNew("Mem Usage: " & x.WorkingSet64 & "k" & vbCrLf & "Paged Memory: " & x.PagedMemorySize64 & " K", False, True)
+        TextBox2.Select()
+    End Sub
+
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        If e.KeyChar = Chr(13) Then
+            newCommand()
+            TextBox2.ResetText()
+        End If
+    End Sub
+
+
+    Private loader
+    Private autoclear = False
+    Private CurrentCommand(1)
+    Private Sub newCommand()
+        If Not CurrentCommand(0) = True Then
+            Select Case TextBox2.Text.ToLower
+                Case "print"
+                    CurrentCommand(0) = True
+                    CurrentCommand(1) = "print"
+                    writeNew("print > Enter text to print:", False, True)
+                Case "exit"
+                    End
+                Case "clear"
+                    writeNew("", False, True)
+                Case "cls"
+                    writeNew("", False, True)
+                Case "set"
+                    CurrentCommand(0) = True
+                    CurrentCommand(1) = "set"
+                    writeNew("Select object to set", False, True)
+                Case "load"
+                    CurrentCommand(0) = True
+                    CurrentCommand(1) = "load"
+                    writeNew("load > Select object to load", False, True)
+                Case "getmem"
+                    Dim x As Process = Process.GetCurrentProcess()
+                    writeNew("Mem Usage: " & x.WorkingSet64 & "k" & vbCrLf & "Paged Memory: " & x.PagedMemorySize64 & " K", True, True)
+                Case Else
+                    writeNew(CurrentCommand(1) & " > " & TextBox2.Text & ": Intent unknown.", True, True)
+            End Select
+        Else
+            Select Case CurrentCommand(1)
+                Case "print"
+                    writeNew(TextBox2.Text, False, True)
+                    CurrentCommand(0) = False
+                Case "set"
+                    Select Case TextBox2.Text.ToLower
+                        Case "autoclear"
+                            If autoclear = True Then
+                                autoclear = False
+                            Else
+                                autoclear = True
+                            End If
+                            writeNew("autoclear > Toggled AutoClear to: " & autoclear, False, True)
+                            CurrentCommand(0) = False
+                    End Select
+                Case "load"
+                    Select Case TextBox2.Text.ToLower
+                        Case "splash x2"
+
+                        Case "map designer"
+                        Case Else
+                            writeNew("load > Unable to locate object. Exiting load.", True, True)
+                            Exit Sub
+                    End Select
+                    writeNew("Load > Sucsessfully Loaded:" & TextBox2.Text, True, True)
+            End Select
+        End If
+
+    End Sub
+
+
+    Private WriteMemory(1, 100)
+    Private Sub writeNew(Line, append, write)
+        If write = False Then
+            WriteMemory(1, 1) += 1
+            WriteMemory(0, WriteMemory(1, 1)) = Line
+        Else
+            If autoclear = True Then
+                TextBox1.ResetText()
+            End If
+            WriteMemory(1, 1) += 1
+                WriteMemory(0, WriteMemory(1, 1)) = Line
+                If append = False Then
+                    TextBox1.ResetText()
+                End If
+                For i = 1 To WriteMemory(1, 1) Step 1
+                    TextBox1.Text += vbCrLf & WriteMemory(0, i)
+                Next
+
+                WriteMemory(1, 1) = 0
+            End If
+    End Sub
+End Class
